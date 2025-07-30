ac9d5a7dd2ddaa578a4a12490a90943929e207b5
PrajyotDabhade <PrajyotDabhade@ISIPL-PUNV-L803.lan>
2025-07-30 16:03:09 +0530

    PD Changes

---
+++ Test Suites/MDM Test Suite/New Test Suite.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a3111ef2-e580-4c5b-88cb-0f17cc908090</testSuiteGuid>
+   <testCaseLink>
+      <guid>1ccf8903-7c3c-4cb3-8af8-681051d3c880</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MDM TestCases/TC_CDM_034_Supplier and Customer read only fields</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ae5a7613-d01e-4056-9d60-f84af6aadd31</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MDM TestCases/TC_CDM_029_Customer - Admin Address Screen</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>99ca63a7-eaac-4c8d-a59d-d0710cb4271f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MDM TestCases/TC_MDM_CDM_314_Deactivate a customer</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
