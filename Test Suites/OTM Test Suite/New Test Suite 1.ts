7ebb71bdafa2661dc907251db17bece8777a03c1
GauravPrasad <GauravPrasad@192.168.1.34>
2025-07-30 18:14:30 +0530

    changes

--- Test Suites/OTM Test Suite/New Test Suite 1.ts
+++ Test Suites/OTM Test Suite/New Test Suite 1.ts
@@ -12,231 +12,161 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>d42978b5-3457-41a1-8973-e00ebe7db907</testSuiteGuid>
    <testCaseLink>
-      <guid>60fe73c5-1c64-409f-a37e-f0e15daf5989</guid>
+      <guid>0d425177-98be-4af7-b7f4-d7387a42ea59</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_004A_P2</testCaseId>
+      <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_003E_P2</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>162316f2-c70c-4869-af5d-82c64e638852</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_015C</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>ef0c587d-9436-4d4c-902c-bd7c7cadf364</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_016A_P2</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>24ce5b5e-e423-4149-a2bd-1e43f793b30b</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_016B_P2</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>b885ed13-cb0f-471e-b793-1854bfc6777b</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_017A_P2</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>c716de76-133d-45e3-bdfd-ca0b1e23a02f</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_017B_P2</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>6e50eaac-d164-40b2-8ed8-2079a24fea3c</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_041_P2</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>4c43a7fb-1f3a-4832-ab67-bb0d4314223f</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_052_P2</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>e75f4359-e465-47af-a20e-246c08a6a185</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_310_P3</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>f11c8db2-fb53-4501-ac1c-904ea7e8abf0</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_312_P3</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>1686f430-3837-4c44-b6a0-7483d310dc8c</guid>
+      <guid>307c95b6-05f1-4c3c-b787-eebf14bdb9e4</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_313_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>9c5b5202-72a4-40f6-a063-f191f4508b7a</guid>
+      <guid>48aca1ba-a66c-4e12-9263-d43ca704ab57</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_321_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>d44407c2-5e53-4c7a-98b1-e6629f2a8fa7</guid>
+      <guid>edfbb8d0-4fa7-479f-8194-1e74dfbe40af</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_324_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>22188b16-1a21-42b8-ab78-c4d172a8ffe8</guid>
+      <guid>fe297fe3-88c9-452c-9190-66b42628043c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_326_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>2e7f76c4-2828-4f47-b5bf-eb855cb09949</guid>
+      <guid>81c73936-bbd6-4b97-893d-54f3f3f8d3bb</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_332_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>b0eca06c-d7fb-4a16-92c0-71d24fd2501c</guid>
+      <guid>b6066604-b739-46e8-a826-aca72cf9e832</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_333_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>33f7dbe2-e439-4a5b-b92e-995875849203</guid>
+      <guid>b743beec-f0c0-4ed6-9fc9-2cac7ebe8627</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_334_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>aef0f4d6-9b4f-4542-910a-0ce32c1126e5</guid>
+      <guid>e2c59ea7-2c2a-4b56-ae26-47f18261bcd7</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_337_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>5917f5cc-a49d-4806-a98a-1afddba54a65</guid>
+      <guid>4af787f8-5634-47f0-a254-b9874aef13e7</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_338_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>31493a94-31f7-4ed9-b55b-5bfea5df2c87</guid>
+      <guid>46960e9a-2d17-4086-916d-af516e164f1d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_339_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>6bfdc006-b9aa-4ce1-a13c-2d43565952d0</guid>
+      <guid>d008fb10-10d4-4fa2-874e-ec5569492049</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_347_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>250527de-a1e0-4c01-961e-a375a6bad476</guid>
+      <guid>1e42e2e0-678c-41f4-b2a9-bff09b86b50b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_350_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>a4f11db7-31a5-4cc7-8809-78240fcdfa02</guid>
+      <guid>93dfe7ee-22e6-4884-9d84-c4a71afebbc6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_351_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>0318f245-1960-41b3-a22f-f2b7e0e1b477</guid>
+      <guid>9e36ca72-25e6-4b9a-b12c-df7352fb47f3</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_352_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>c22a59a9-f1c5-4fc5-b935-2e716631f64a</guid>
+      <guid>28e51705-05ff-47bc-9860-e1e4f20af31e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_353_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>5ab9ebae-2b99-41a7-983f-41bd29ca02c6</guid>
+      <guid>04bbfa95-83d0-4a92-b81c-8f7039c7db21</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_354_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>8a644280-fee7-4e80-8bfb-a9ec083f14d4</guid>
+      <guid>94e90206-9474-4533-949e-700321c744fb</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_355_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>543ef730-fa68-49fb-bc58-55f833ee80dc</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_358_P3</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>e8c3b2a2-463a-4d6b-a378-05ab21fcce3b</guid>
+      <guid>2d069e85-3f4c-4fa7-aa40-d2f29a35c820</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_360_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>b720857a-be2a-4c15-9cf1-c0b02471ec39</guid>
+      <guid>9a4c17cb-1dad-4d4a-8b71-4bc3c63d4f3b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_362_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>698d2f3b-9bf4-4f44-b36c-88d8b525df2c</guid>
+      <guid>2455ee9b-3161-403b-a9de-535d36e1b0fc</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_365_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>1291cfdf-d895-4365-b2ce-2201a2f1f716</guid>
+      <guid>d1cda2ba-6296-4464-92dd-a034a3929fcd</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/TC_OTM_367_P3</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>248162ca-c08b-4796-acd2-f168155924de</guid>
+      <guid>01c16550-4ae9-4a0b-9491-df3546b818e8</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/OTM_Test_Cases/Tracking_Events_TC_OTM_322</testCaseId>
