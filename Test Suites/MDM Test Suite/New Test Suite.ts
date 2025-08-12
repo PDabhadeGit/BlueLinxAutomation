03c1a3e71d6d0dee4feb6c1253e3d28790269619
PDabhadeGit <Prajyot.Dabhade@BlueLinxCo.com>
2025-08-12 18:59:38 +0530

    New PD

--- Test Suites/MDM Test Suite/New Test Suite.ts
+++ Test Suites/MDM Test Suite/New Test Suite.ts
@@ -12,24 +12,17 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>a3111ef2-e580-4c5b-88cb-0f17cc908090</testSuiteGuid>
    <testCaseLink>
-      <guid>1ccf8903-7c3c-4cb3-8af8-681051d3c880</guid>
+      <guid>03d304b1-a2c7-4698-a4e1-fe232b88aaac</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MDM TestCases/TC_CDM_034_Supplier and Customer read only fields</testCaseId>
+      <testCaseId>Test Cases/MDM TestCases/TC_MDM_CDM_302_Update Supplier information via mass-load</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>ae5a7613-d01e-4056-9d60-f84af6aadd31</guid>
+      <guid>1cc16a65-9d37-429e-8d2c-325675441568</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MDM TestCases/TC_CDM_029_Customer - Admin Address Screen</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>99ca63a7-eaac-4c8d-a59d-d0710cb4271f</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/MDM TestCases/TC_MDM_CDM_314_Deactivate a customer</testCaseId>
+      <testCaseId>Test Cases/MDM TestCases/TC_MDM_CDM_304_Update Customer information via mass-load</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
