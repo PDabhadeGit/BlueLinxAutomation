657b05c3b7b09853e8d72305fbfd12f27b7b2007
PDabhadeGit <Prajyot.Dabhade@BlueLinxCo.com>
2025-09-22 23:00:01 +0530

    New

--- Test Suites/MDM Test Suite/MDM approve item.ts
+++ Test Suites/MDM Test Suite/MDM approve item.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>test</name>
+   <name>MDM approve item</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
